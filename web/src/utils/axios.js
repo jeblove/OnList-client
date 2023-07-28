2cc38addb2d50c3fb1da74ed51c22b05d3cf08c7
jeblove <249972068@qq.com>
2023-07-28 13:52:07 +0800

    host环境变量

--- web/src/utils/axios.js
+++ web/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL
+axios.defaults.baseURL = import.meta.env.VITE_APP_URL || "http://localhost:8080"
 
 const instance = axios.create({
     timeout: 10000, // 超时时间
