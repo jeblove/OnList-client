b72e65624976a790b250d1a589843382b42b76ca
jeblove <249972068@qq.com>
2023-07-28 18:00:39 +0800

    修复dist变量问题

--- web/src/utils/axios.js
+++ web/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL || "http://localhost:8080"
+// axios.defaults.baseURL = import.meta.env.VITE_APP_URL
+axios.defaults.baseURL = "http://localhost:8080"
 
 const instance = axios.create({
     timeout: 10000, // 超时时间
