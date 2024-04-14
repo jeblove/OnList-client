3b77198628e80553f90c07a07e3951464532e567
jeblove <9rHZkvGC4of@wJ>
2024-04-14 16:22:19 +0800

    update: JSON格式化显示

--- web/src/components/setting/Path.vue
+++ web/src/components/setting/Path.vue
@@ -1,29 +1,27 @@
 <template>
-  <el-input
-    v-model="textarea"
-    style="width: 240px"
-    autosize
-    type="textarea"
-    placeholder="Please input"
-  />
+    <vue-json-pretty :data="json_data" deep="3" showLength="true" showLineNumber="true" showIcon="true"
+        virtual="true" />
 </template>
 
 <script>
 import axios from '@/utils/axios';
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+
 export default {
     components: {
-        
+        VueJsonPretty,
     },
     data() {
         return {
-            textarea: '',
+            json_data: '',
         };
     },
     async created() {
         const res = await axios.post('/path/getAllPathInfo');
         console.log(res.data.data)
         if (res.data.code === 200) {
-            this.textarea = JSON.stringify(res.data.data);
+            this.json_data = res.data.data;
         } else {
             this.$message.error('获取目录信息失败:' + res.data.message);
         }
